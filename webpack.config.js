42515f7266b971e7f5e1b0cb051aaf22af064d8a
Stefano-Sega <stefano@prototype.ae>
2017-03-04 23:12:55 +0400

    Handled minification based on environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
+var getPlugins = function(isProduction) {
+  var plugins = [
+      new webpack.HotModuleReplacementPlugin()
+    ];
+
+  if (isProduction) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+  }
+
+  return plugins;
+}
+
 var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'public/dist');
 var APP_DIR = path.resolve(__dirname, 'public');
+var isProduction = process.env.NODE_ENV === "production";
 
 var config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
-    publicPath: BUILD_DIR,
     filename: 'bundle.js'
   },
   module : {
@@ -20,9 +32,11 @@ var config = {
       }
     ]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
-  ]
+  plugins: getPlugins(isProduction),
+  devServer: {
+    contentBase: BUILD_DIR,
+    hot: true
+  }
 };
 
 module.exports = config;
